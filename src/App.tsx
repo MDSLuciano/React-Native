0379a9e3886384adef789ce31136c685839e85f1
Luciano Machado <lucianoo_machado@hotmail.com>
2025-03-08 12:21:50 -0300

    Using Button

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { styles } from './components/StyleSheets';
 import MinMax from './components/MinMax';
 import RandomNumber from './components/RandomNumber';
 import Title from './components/Title';
+import Button from './components/Button';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Title title="Cadastro de Produtos" subtitle="Tela de cadastro" />
+      <Button title="Executar"/>
       {/* <MinMax x={10} y={0} />
+      <Title title="Cadastro de Produtos" subtitle="Tela de cadastro" />
+      <Button title="Cadastrar" />
       <MinMax x={0} y={10} /> 
       <RandomNumber min={1} max={50} />
       <RandomNumber min={1} max={50} />
---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button } from 'react-native';
+
+import { styles } from './StyleSheets';
+
+interface ButtonProps {
+    title: string;
+}
+
+export default (props: ButtonProps) => {
+    function execute() {
+        console.warn('Executando...')
+    }
+    return (
+    
+    <>
+        <Button 
+            title="Executar 01"
+            onPress={execute}
+        />
+        <Button 
+            title="Executar 02"
+            onPress={function() {console.warn('Executando 02...')}}
+        />
+        <Button 
+            title="Executar 03"
+            onPress={() => console.warn('Executando 03...')}
+        />
+    </>
+  );
+}
